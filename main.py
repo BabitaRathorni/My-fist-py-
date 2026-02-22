2141152736536cd0000c6e5233da51a1919aec90
Babita-RAju <rathorbabita41@gmail.com>
2026-02-22 13:07:53 +0530

    Implement custom Sqlite3Recipe for SQLite
    Add custom Sqlite3Recipe class with version and URL.

--- main.py
+++ main.py
@@ -2,7 +2,20 @@ import re
 import math
 import random
 import json
+from pythonforandroid.recipes.sqlite3 import Sqlite3Recipe
+from pythonforandroid.util import load_source
 import os
+
+class Sqlite3Recipe(Sqlite3Recipe):
+    version = '3.42.0'
+    url = 'https://www.sqlite.org/2023/sqlite-autoconf-3420000.tar.gz'
+    
+    def get_recipe_env(self, arch):
+        env = super().get_recipe_env(arch)
+        env['CFLAGS'] += ' -DSQLITE_ENABLE_COLUMN_METADATA=1'
+        return env
+
+recipe = Sqlite3Recipe()
 import hashlib
 import pickle
 import sqlite3
